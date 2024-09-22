ebca73e305398e7cabb1eed0f351c8bca60c48d1
Gabriel Xavier Pereira <gabrielxp2006@gmail.com>
2024-09-22 00:29:59 -0300

    Manutenção do PHP
    Manutenção do sistema de login e registro da parte PHP do projeto

--- php/login.php
+++ php/login.php
@@ -3,7 +3,7 @@ session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Conectar ao banco de dados
-    $conn = new mysqli("localhost", "Sotby", "Gxp142536", "users");
+    $conn = new mysqli("localhost", "Sotby", "142536", "users");
 
     if ($conn->connect_error) {
         die("Erro na conexão: " . $conn->connect_error);
@@ -15,13 +15,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $senha = $_POST['psw'];
 
     // Verificar se o usuário existe
-    $sql = "SELECT * FROM usuarios WHERE email = '$email' or username = '$username'" ;
+    $sql = "SELECT * FROM userinfo WHERE email = '$email' or username = '$username'" ;
     $result = $conn->query($sql);
     
     if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
         // Verificar senha
-        if (password_verify($senha, $row['senha'])) {
+        if (password_verify($senha, $row['pass'])) {
             $_SESSION['username'] = $row['username'];
             header("Location: dashboard.php");  // Redirecionar para a página de dashboard
         } else {
--- php/register.php
+++ php/register.php
@@ -1,7 +1,7 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Conectar ao banco de dados
-    $conn = new mysqli("localhost", "Sotby", "Gxp142536", "users");
+    $conn = new mysqli("localhost", "Sotby", "142536", "users");
     
     if ($conn->connect_error) {
         die("Erro na conexão: " . $conn->connect_error);
@@ -13,19 +13,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $senha = password_hash($_POST['psw'], PASSWORD_DEFAULT);
 
     // Verificar se o usuário já existe
-    $sql = "SELECT * FROM usuarios WHERE email = '$email'";
+    $sql = "SELECT * FROM userinfo WHERE email = '$email'";
     $result = $conn->query($sql);
     
     if ($result->num_rows > 0) {
         echo "Email já registrado!";
     } else { 
-        $sql = "SELECT * FROM usuarios WHERE username = '$username'";
+        $sql = "SELECT * FROM userinfo WHERE username = '$username'";
         $result = $conn->query(query: $sql);
         if($result->num_rows>0){
             echo "Nome de usuario em uso";
         } else {
         // Inserir no banco de dados
-        $sql = "INSERT INTO usuarios (username, email, pass) VALUES ('$username', '$email', '$senha')";
+        $sql = "INSERT INTO userinfo (username, email, pass) VALUES ('$username', '$email', '$senha')";
         if ($conn->query($sql) === TRUE) {
             echo "Registro concluído com sucesso!";
             header("Location: login.php");
